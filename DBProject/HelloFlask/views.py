114636ccdf9382b485093d829f2b622b619ca3e6
DEBORAH <deborahdebiasio@icloud.com>
2020-12-07 12:12:53 +0100

    Commit Navbar Gestore and Bootstrap folder

--- DBProject/HelloFlask/views.py
+++ DBProject/HelloFlask/views.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return "Hello Flask!"
+@app.route('/gestoreHome')
+def gestoreHome():
+    return render_template(
+        "gestoreHome.html",
+        title = "gestoreHome",
+        content = "gestoreHome")
+
+
+
+@app.route('/api/data')
+def get_data():
+  return app.send_static_file('data.json')
+
+@app.route('/gestoreAddMovie')
+def gestoreAddMovie():
+    return render_template(
+        "gestoreAddMovie.html",
+        title = "gestoreAddMovie",
+        content = "gestoreAddMovie")
+
+@app.route('/gestoreAnalytics')
+def gestoreAnalytics():
+    return render_template(
+        "gestoreAnalytics.html",
+        title = "gestoreAnalytics",
+        content = "gestoreAnalytics")
+
+
+@app.route('/gestoreUser')
+def gestoreUser():
+    return render_template(
+        "gestoreUser.html",
+        title = "gestoreUser",
+        content = "gestoreUser")
+
